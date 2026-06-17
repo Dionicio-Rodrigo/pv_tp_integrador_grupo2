07c9c980cce8537a251ff608c7bb502c14a8bae8
Dio <47392641@fi.unju.edu.ar>
2026-06-17 11:17:51 -0300

    Layout basico/borrador

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
+import LayoutPagina from "./components/layout/LayoutPagina";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>Probando</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LayoutPagina />}>
+            {/* Rutas de las otras vistas */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
