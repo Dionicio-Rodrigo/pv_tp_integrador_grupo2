cfbb9952e29eea410b4df5a228d1e77f86fdaafa
Dio <47392641@fi.unju.edu.ar>
2026-06-22 22:54:39 -0300

    Layout Para la pagina

--- src/components/layout/LayoutPagina.jsx
+++ src/components/layout/LayoutPagina.jsx
@@ -1,13 +1,33 @@
-import { Box, Container, Stack } from "@mui/material";
+import { Box, Container, Stack, Paper } from "@mui/material";
+import { Nav } from "./Nav";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
 
 const LayoutPagina = () => {
   return (
     <Stack sx={{ height: "100vh" }}>
-      <Box component="header">Prueba header {/*Componente Header*/}</Box>
-      <Container component="main" sx={{ flexGrow: "1" }}>
-        Prueba main {/*  Aca se colocara el componente Outlet */}
-      </Container>
-      <Box component="footer">Prueba footer {/*Componente Footer*/}</Box>
+      <Box component="header">
+        <Header />
+      </Box>
+      <Box component="main" sx={{ flexGrow: "1", p: "1em" }}>
+        {/* Aca tiene que ir el componente Outlet */}
+        {/* Los componentes que salen ahora son solo ejemplos */}
+        <Stack spacing={1}>
+          <Paper>Probando</Paper>
+          <Paper>Probando</Paper>
+        </Stack>
+      </Box>
+      <Box
+        component="footer"
+        sx={{
+          bgcolor: "error.main",
+          color: "error.contrastText",
+          px: "1em",
+          py: "0.25em",
+        }}
+      >
+        Texto Ejemplo
+      </Box>
     </Stack>
   );
 };
