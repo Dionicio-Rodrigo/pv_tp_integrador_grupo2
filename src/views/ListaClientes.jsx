3d6dfcfd79c8bbceb5bea69e7c98747fd0203424
Dio <47392641@fi.unju.edu.ar>
2026-06-28 13:02:38 -0300

    Busqueda solo se vera cuando la api responda correctamente

--- src/views/ListaClientes.jsx
+++ src/views/ListaClientes.jsx
@@ -1,19 +1,26 @@
-import { Box, TextField, Typography, Alert } from "@mui/material";
+import {
+  Box,
+  TextField,
+  Typography,
+  Alert,
+  Skeleton,
+  Stack,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { SkeletonTabla } from "../components/common/SkeletonTabla";
 import { TablaClientes } from "../components/common/TablaClientes";
 
 const ListaClientes = () => {
   const [usuarios, setUsuarios] = useState(undefined);
-  
-  const [busqueda, setBusqueda] = useState(""); 
+
+  const [busqueda, setBusqueda] = useState("");
 
   useEffect(() => {
     const llamadaApi = async () => {
       try {
         const res = await fetch("https://fakestoreapi.com/users");
         if (!res.ok) throw new Error("Api no responde");
-        
+
         const data = await res.json();
         setUsuarios(data);
       } catch (error) {
@@ -36,35 +43,46 @@ const ListaClientes = () => {
 
   return (
     <Box sx={{ p: 3 }}>
-      {/* LA INTERFAZ DEL BUSCADOR */}
-      <Box sx={{ mb: 4 }}>
-        <Typography variant="h5" sx={{ mb: 2, fontWeight: 'bold' }}>
-          Gestión de Clientes
-        </Typography>
-        <TextField
-          fullWidth
-          label="Buscar cliente por apellido o ciudad..."
-          variant="outlined"
-          value={busqueda}
-          onChange={(e) => setBusqueda(e.target.value)}
-        />
-      </Box>
-
       {/* RENDERIZADO CONDICIONAL */}
-      
+
       {/* Estado: Cargando */}
-      {usuarios === undefined && <SkeletonTabla filas={10} />}
-      
+      {usuarios === undefined && (
+        <Stack spacing={2}>
+          <Skeleton
+            height={100}
+            variant="rounded"
+            sx={{ bgcolor: "warning.main" }}
+          />
+          <SkeletonTabla filas={10} />
+        </Stack>
+      )}
+
       {/* Estado: Error */}
       {usuarios === false && (
-        <Alert severity="error">
-          Error de conexión: No se pudo cargar la lista de clientes desde la API.
+        <Alert severity="error" variant="filled">
+          Error de conexión: No se pudo cargar la lista de clientes desde la
+          API.
         </Alert>
       )}
 
       {/* Estado: Éxito */}
       {Array.isArray(usuarios) && (
-        <TablaClientes clientes={usuariosFiltrados} />
+        <>
+          {/* LA INTERFAZ DEL BUSCADOR */}
+          <Box sx={{ mb: 4 }}>
+            <Typography variant="h5" sx={{ mb: 2, fontWeight: "bold" }}>
+              Gestión de Clientes
+            </Typography>
+            <TextField
+              fullWidth
+              label="Buscar cliente por apellido o ciudad..."
+              variant="outlined"
+              value={busqueda}
+              onChange={(e) => setBusqueda(e.target.value)}
+            />
+          </Box>
+          <TablaClientes clientes={usuariosFiltrados} />
+        </>
       )}
     </Box>
   );
