7fe278400883474d14aece7afa2c5a600c789f20
Armeya Efraain <armeyaefrain@gmail.com>
2026-06-29 15:52:01 -0300

    Implementacion de AdminContext y Persistencia

---
+++ src/context/AdminContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect } from "react";
+export const AdminContext = createContext();
+export const AdminProvider = ({ children }) =>{
+
+    const [admin, setAdmin] = useState(() => {
+        const adminGuardado = localStorage.getItem("admin");
+
+        return adminGuardado
+        ? JSON.parse(adminGuardado)
+        : null;
+    });
+
+    const login = (datos) => {
+        setAdmin(datos);
+    };
+
+    const logout = () => {
+        setAdmin(null);
+        localStorage.removeItem("admin");
+    }
+
+    useEffect(() => {
+        if (admin) {
+            localStorage.setItem(
+                "admin",
+                JSON.stringify(admin)
+            );
+        }
+    }, [admin]);
+
+    return(
+        <AdminContext.Provider
+        value={{
+            admin,
+            login,
+            logout
+        }}
+        >
+            {children}
+        </AdminContext.Provider>
+    );
+};
+
