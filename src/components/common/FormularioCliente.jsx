9ff76d52b32fc07f058029c9250fcc5e68f8ef58
Armeya Efraain <armeyaefrain@gmail.com>
2026-07-05 19:59:35 -0300

    Implemento formik en el formulario clientes

--- src/components/common/FormularioCliente.jsx
+++ src/components/common/FormularioCliente.jsx
@@ -1,42 +1,40 @@
-import { useState } from "react";
+import {Formik,Form} from "formik";
 import {Paper,Typography,TextField,Button,Stack} from "@mui/material";
 
 
 const FormularioCliente = () => {
 
-    const clienteInicial = {
-        nombre: "", apellido: "", email: "", telefono: "", ciudad: "",
-    };
-    const [cliente,setCliente] = useState(clienteInicial);
-    const handleChange = (e) => {
-        setCliente({
-            ...cliente,
-            [e.target.name]: e.target.value,
-        });
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(cliente);
-        setCliente(clienteInicial);
-    };
+    const initialValues = {
+        nombre:"",apellido:"",email:"",telefono:"",ciudad:"",};
+
+        const manejarEnvio = (values, {resetForm}) => {
+            console.log(values);
+
+            resetForm();
+        }
+    
     return (
-        <Paper
-        component="form"
-        onSubmit={handleSubmit} 
-        sx={{p:3,mb:4}}>
+        <Formik initialValues={initialValues} onSubmit={manejarEnvio}>
+            {({ values, handleChange }) => (
+                <Paper sx={{p:3,mb:4}}>
+                    <Form noValidate>
+
             <Typography variant="h6">
                 Alta de cliente
             </Typography>
 
             <Stack spacing={2}>
-                <TextField label="Nombre" name="nombre" value={cliente.nombre} onChange={handleChange} fullWidth/>
-                <TextField label="Apellido" name="apellido" value={cliente.apellido} onChange={handleChange} fullWidth/>
-                <TextField label="Email" name="email" value={cliente.email} onChange={handleChange} fullWidth/>
-                <TextField label="Telefono" name="telefono" value={cliente.telefono} onChange={handleChange} fullWidth/>
-                <TextField label="Ciudad" name="ciudad" value={cliente.ciudad} onChange={handleChange} fullWidth/>
-                <button type="submit" variant="contained">Guadar Cliente</button>
+                <TextField label="Nombre" name="nombre" value={values.nombre} onChange={handleChange} fullWidth/>
+                <TextField label="Apellido" name="apellido" value={values.apellido} onChange={handleChange} fullWidth/>
+                <TextField label="Email" name="email" value={values.email} onChange={handleChange} fullWidth/>
+                <TextField label="Telefono" name="telefono" value={values.telefono} onChange={handleChange} fullWidth/>
+                <TextField label="Ciudad" name="ciudad" value={values.ciudad} onChange={handleChange} fullWidth/>
+                <Button type="submit" variant="contained">Guardar Cliente</Button>
             </Stack>
+            </Form>
         </Paper>
+            )}
+        </Formik>
     );
 };
 
